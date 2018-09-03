f25ad72247721f8065aa5a014a7bd194d9fc8f49
Elmir I-de <kryternext@gmail.com>
2018-09-03 17:20:21 +0400

    Simple configuring urls and views

--- excel_handler/urls.py
+++ excel_handler/urls.py
@@ -1,1 +1,9 @@
 from django.urls import path
+from . import views
+
+app_name = "excel_handled"
+
+urlpatterns = [
+    path('', views.HelloWorld.as_view(), name='index'),
+    # path('hello/', views.HelloWorld.as_view(), name="hello")
+]
--- excel_handler/views.py
+++ excel_handler/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import View
+from django.http import HttpResponse
+
 
 # Create your views here.
+class HelloWorld(View):
+    def get(self, request):
+        return HttpResponse("<h1>Hello world!</h1>")
